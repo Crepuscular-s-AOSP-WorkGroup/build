e0e5217008b781394ee454c7eea811c5ff48dc89
dianlujitao <dianlujitao@lineageos.org>
2019-09-05 21:56:36 +0800

    product: Exclude AOSP apn list on custom build
    Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898

--- core/config.mk
+++ core/config.mk
@@ -317,7 +317,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 ifneq ($(CUSTOM_BUILD),)
-include vendor/aosp/config/BoardConfigFluid.mk
+include vendor/aosp/config/BoardConfigAosp.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
