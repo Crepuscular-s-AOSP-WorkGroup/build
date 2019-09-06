cf04e4114a73001574c6b0bcd2398c6cd0338c9f
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-05 21:35:38 -0700

    core: Only include custom sepolicy if repo exists
    Change-Id: If799106d2da6d8c303430e316edff365a22f7c99

--- core/config.mk
+++ core/config.mk
@@ -1255,6 +1255,14 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+ifneq ($(CUSTOM_BUILD),)
+ifneq ($(wildcard device/custom/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/custom/sepolicy/common/sepolicy.mk)
+endif
+endif
+
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
 
